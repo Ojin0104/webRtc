7316b403f97dd16bccde04d0fc951e47ebb78d95
yeongjin han <hanyj0104@naver.com>
2022-08-08 21:23:59 +0900

    ing

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,14 @@
-hello
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open",()=>{
+    console.log("Connected to Server v");
+});
+
+socket.addEventListener("message",(message)=>{
+    console.log("Just got this :",message, " from the Server");
+});
+
+socket.addEventListener("close",()=>{
+    console.log("Connected from Server ");
+});
+
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express from 'express';
 import http from "http";
-import WebSocket from "ws";
+import { WebSocketServer } from 'ws';
+import path from 'path';
 const app=express();
-
+ const __dirname=path.resolve();
 app.set("view engine","pug");
 app.set("views",__dirname + "/views");
 app.use("/public",express.static(__dirname+"/public"));
@@ -13,6 +14,20 @@ const handleListen=() => console.log('Listening on http://localhost:4000');
 
 const server=http.createServer(app);
 
-const wss=new WebSocket.Server({ server} );
+const wss=new WebSocketServer({ server} );
+
+
+   
+
+
+wss.on("connection",(socket)=>{
+    console.log("Connected to Browser v")
+    socket.on("close",()=> console.log("Disconnected from the BRowser"));
+    socket.on("message",(message)=>{
+        console.log(message);
+    })
+    socket.send("hello!!");
+});
+
 
 server.listen(4000,handleListen);
